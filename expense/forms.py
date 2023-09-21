c6fd16228df397c984bea994d6725b069cc2efe6
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-09-21 16:59:06 +0600

    update forms.py

--- expense/forms.py
+++ expense/forms.py
@@ -5,6 +5,7 @@ from .models import Expense
 
 
 class ExpenseAdminForm(ModelForm):
+    # some works need to be done
 
     class Meta:
         model = Expense
@@ -21,6 +22,13 @@ class ExpenseAdminReportForm(forms.Form):
 
     def clean(self):
         data =  super().clean()
-        print("data ", data)
+        date1 = data.get("date1")
+        date2 = data.get("date2")
+        if date1 == date2:
+            raise forms.ValidationError("Date mismatched")
+        
+        if date1 > date2:
+            raise forms.ValidationError("date2 cannot be more than date1")
+        
         return data
 
