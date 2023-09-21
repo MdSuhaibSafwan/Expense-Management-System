772e723ab77f403f8e0602ef7bda4827cdf0db16
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-09-21 16:15:58 +0600

    update views.py

---
+++ expense/utils.py
@@ -0,0 +1,6 @@
+from .models import Expense
+from django.db.models import Q
+
+def get_expense_from_dates(date1, date2):
+    qs = Expense.objects.all() #.filter(Q(date_created__gte=date1) & Q(date_created__lte=date2))
+    return qs
--- expense/views.py
+++ expense/views.py
@@ -2,14 +2,34 @@ from django.shortcuts import redirect
 from .models import Expense
 from .forms import ExpenseAdminReportForm
 from django.contrib import messages
+from .utils import get_expense_from_dates
+from django.http import HttpResponse
+from openpyxl import Workbook
 
 
 def create_xl_report_for_expense(request):
     form = ExpenseAdminReportForm(data=request.POST)
     if not form.is_valid():
-        print(form.errors)
-        messages.error(request, message="Provide date")
+        messages.error(request, message="Please provide date correctly")
+        return redirect("/admin/expense/expense/", )
     
-    return redirect("/admin/expense/expense/", )
+    date1 = form.cleaned_data.get("date1")
+    date2 = form.cleaned_data.get("date2")
+    qs = get_expense_from_dates(date1, date2)
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment;filename="report.xlsx"'
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Expenses"
+
+    # Add headers
+    headers = ["Name", "Price", "Quantity", "Total Cost"]
+    ws.append(headers)
+
+    for expense in qs:
+        ws.append([expense.category.name, expense.cost, expense.quantity, expense.total_cost(), ])
+
+    wb.save(response)    
+    return response
 
 
