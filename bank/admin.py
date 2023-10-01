1fb835367d7c12364ead542958c7186fb3d9dc9e
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-10-01 17:50:10 +0600

    update admin.py in banks

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CashCheckout, RegisteredBank
 
-# Register your models here.
+
+class CashCheckoutAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "bank"]
+
+
+class BankAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "open_balance"]
+
+
+admin.site.register(CashCheckout, CashCheckoutAdmin)
+admin.site.register(RegisteredBank, BankAdmin)
