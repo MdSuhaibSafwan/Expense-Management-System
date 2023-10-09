376b61946ab050a62a4533db8ad88b361943bea7
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-10-09 13:33:33 +0600

    add validation to forms

--- account/forms.py
+++ account/forms.py
@@ -19,5 +19,20 @@ class FundTransferForm(forms.ModelForm):
 		if from_account.get_current_balance() < amount:
 			raise forms.ValidationError("Insufficient Balance")
 
+		self.validate_from_account(data)
+
 		return data
+
+	def validate_from_account(self, data):
+		from_account = data["from_account"]
+		fund_transfer_obj = from_account.get_latest_outgoing_fund_transfer()
+		if fund_transfer_obj is None:
+			return from_account
+
+		if not ((fund_transfer_obj.is_approved()) and (fund_transfer_obj.is_checked())):
+			self.add_error("from_account", "Please Approve the previous Fund Transaction to apply again")
+
+		return from_account
+
+
 
