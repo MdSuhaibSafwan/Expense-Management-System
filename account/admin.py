399a19c491d69881178a98020cd0feeb92646bb8
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-10-09 16:08:17 +0600

    update admin.py

--- account/admin.py
+++ account/admin.py
@@ -4,6 +4,16 @@ from lib.admin import BaseAdmin
 from .forms import FundTransferForm
 
 
+class FundApproveInline(admin.TabularInline):
+	model = FundApprove
+	fields = ["description", "is_approved"]
+
+
+class FundCheckInline(admin.TabularInline):
+	model = FundCheck
+	fields = ["description", "is_checked"]
+
+
 class FundTransferAdmin(BaseAdmin):
 	form = FundTransferForm
 	list_display = ["id", "from_account", "to_account", "amount", "is_approved", "is_checked"]
@@ -13,6 +23,34 @@ class FundTransferAdmin(BaseAdmin):
 	)
 
 	add_fieldsets = fieldsets
+
+	def get_form(self, *args, **kwargs):
+		form = super().get_form(*args, **kwargs)
+		form.action_view = self.action_view
+		return form
+
+	def add_view(self, request, *args, **kwargs):
+		self.action_view = "add_view"
+		self.add_tabular_inline_according_to_user(request.user)
+		self.add_readonly_fields_according_to_user(request.user)
+		return super().add_view(request, *args, **kwargs)
+
+	def change_view(self, request, *args, **kwargs):
+		self.action_view = "change_view"
+		self.add_readonly_fields_according_to_user(request.user)
+		self.add_tabular_inline_according_to_user(request.user)
+		return super().change_view(request, *args, **kwargs)
+
+	def add_readonly_fields_according_to_user(self, user):
+		if not user.is_author:
+			self.readonly_fields = ["from_account", "to_account", "amount", "description"]
+
+	def add_tabular_inline_according_to_user(self, user):
+		if user.is_checker:
+			self.inlines = [FundCheckInline, ]
+
+		if user.is_approver:
+			self.inlines = [FundApproveInline, ]
 
 
 class AccountAdmin(BaseAdmin):
