7f39042e1a9e19c8ac02b7eaead685ab0da3022e
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-10-09 13:57:34 +0600

    update validation in forms.py

--- account/forms.py
+++ account/forms.py
@@ -29,10 +29,24 @@ class FundTransferForm(forms.ModelForm):
 		if fund_transfer_obj is None:
 			return from_account
 
-		if not ((fund_transfer_obj.is_approved()) and (fund_transfer_obj.is_checked())):
-			self.add_error("from_account", "Please Approve the previous Fund Transaction to apply again")
+		# if not ((fund_transfer_obj.is_approved()) and (fund_transfer_obj.is_checked())):
+		# 	self.add_error("from_account", "Please Approve the previous Fund Transaction to apply again")
 
+		self.validate_checking_response(fund_transfer_obj)
+		self.validate_approval_response(fund_transfer_obj)
 		return from_account
 
+	def validate_checking_response(self, fund_transfer_obj):
+		checker_resp_obj = fund_transfer_obj.get_checking_response()
+		if checker_resp_obj is None:
+			self.add_error("from_account", "Please provide a check to fund transaction to apply again from this account")
 
+		return True
+
+	def validate_approval_response(self, fund_transfer_obj):
+		approval_response_obj = fund_transfer_obj.get_approval_response()
+		if approval_response_obj is None:
+			self.add_error("from_account", "Please approve previous fund transaction to apply again from this account")
+
+		return True
 
