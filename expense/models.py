ec23b1da189505137cc1994fa3527d4953218df8
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-10-12 14:39:54 +0600

    update expense/models.py

--- expense/models.py
+++ expense/models.py
@@ -6,11 +6,9 @@ from lib.models import BaseModel
 User = get_user_model()
 
 
-class Category(models.Model):
+class Category(BaseModel):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
