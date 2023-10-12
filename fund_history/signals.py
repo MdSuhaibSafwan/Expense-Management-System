3b9d67c77a7e601a543163f583433f2373e6f55a
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-10-12 17:32:31 +0600

    add signals in fund_history app

---
+++ fund_history/signals.py
@@ -0,0 +1,26 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_delete
+from django.contrib.contenttypes.models import ContentType
+from account.dispatch import fund_transfer_approved
+from account.models import FundTransfer
+from expense.models import Expense
+from .models import FundHistory
+
+
+@receiver(signal=post_save, sender=Expense)
+def add_an_instruction_to_journal_for_expense(sender, instance, created, **kwargs):
+	if not created:
+		return None
+
+	content_type = ContentType.objects.get_for_model(Expense)
+	fh_obj = FundHistory(object_id=instance.id, content_type=content_type)
+	fb_obj.transaction_code = instance.id
+	fh_obj.save()
+
+
+@receiver(signal=fund_transfer_approved)
+def add_an_instruction_to_journal_for_account_fund_transfer(sender, instance, **kwargs):
+	content_type = ContentType.objects.get_for_model(instance.__class__)
+	fh_obj = FundHistory(object_id=instance.id, content_type=content_type)
+	fb_obj.transaction_code = instance.id
+	fh_obj.save()
