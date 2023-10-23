228144f76cd69ffbe125f168ab259eec4396780a
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-10-23 11:57:00 +0600

    update setup-2fa html page

--- admin_site/admin.py
+++ admin_site/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.contrib.admin import *
+from user.views import TwoFactorAuthSetupView
+from django.urls import path
 
 
 class AdminSiteConfig(admin.AdminSite):
 	site_header = "Expense Management"
+
+	def get_urls(self, *args, **kwargs):
+		previous_urls = super().get_urls(*args, **kwargs)
+		urls = [
+			path("setup-2fa/", self.admin_view(TwoFactorAuthSetupView.as_view()), name="setup_2fa")
+		] + previous_urls
+		return urls
 
 
 site = AdminSiteConfig(name="Expense Management")
