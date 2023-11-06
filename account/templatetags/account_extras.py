036e9f721f7654f78abf202a1d4f84442fee8782
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-11-06 19:29:21 +0600

    add icon to delete and change

--- account/templatetags/account_extras.py
+++ account/templatetags/account_extras.py
@@ -4,6 +4,16 @@ register = template.Library()
 
 
 @register.simple_tag
-def check_user_perm_for_model(user, app_name, model_name):
+def check_user_perm_for_model_to_add(user, app_name, model_name):
+	return user.has_perm(f"{app_name}.add_{model_name}")
+
+
+
+@register.simple_tag
+def check_user_perm_for_model_to_change(user, app_name, model_name):
 	return user.has_perm(f"{app_name}.change_{model_name}")
 
+
+@register.simple_tag
+def check_user_perm_for_model_to_delete(user, app_name, model_name):
+	return user.has_perm(f"{app_name}.delete_{model_name}")
