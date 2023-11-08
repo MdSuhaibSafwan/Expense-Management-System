20f35a96d1251abb1a7e3146a1a2409c29c22e3f
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-11-08 15:11:17 +0600

    add unique to category name

--- expense/models.py
+++ expense/models.py
@@ -23,7 +23,7 @@ class CategoryManager(models.Manager):
 
 
 class Category(BaseModel):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
 
     objects = CategoryManager()
