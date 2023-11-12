94e2b49c4837794592988e92147f4877d97ee841
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-11-12 18:01:46 +0600

    update utils for report

--- account/utils.py
+++ account/utils.py
@@ -14,7 +14,7 @@ def report_for_an_account(account):
 	ledger_list = []
 
 	qs = FundTransfer.objects.filter(from_account=account)
-	from_acc_temp_lst = list(qs.values_list("date_created", "id", "amount"))
+	from_acc_temp_lst = list(qs.values_list("date_created", "to_account__name", "id", "amount"))
 	temp_lst = []
 	for i in from_acc_temp_lst:
 		i = list(i)
@@ -24,7 +24,7 @@ def report_for_an_account(account):
 	ledger_list.extend(temp_lst)
 
 	qs = FundTransfer.objects.filter(to_account=account)
-	to_acc_temp_lst = list(qs.values_list("date_created", "id", "amount"))
+	to_acc_temp_lst = list(qs.values_list("date_created", "from_account__name", "id", "amount"))
 	temp_lst = []
 	for i in to_acc_temp_lst:
 		i = list(i)
@@ -33,7 +33,7 @@ def report_for_an_account(account):
 
 	ledger_list.extend(temp_lst)
 
-	expense_temp_lst = list(Expense.objects.filter(account=account).values_list("date_created", "id", "cost"))	
+	expense_temp_lst = list(Expense.objects.filter(account=account).values_list("date_created", "title", "id", "cost"))	
 	for i in expense_temp_lst:
 		i = list(i)
 		i.append(True)
@@ -47,5 +47,3 @@ def report_for_an_account(account):
 
 	sorted_ledger_list = sorted(ledger_list, key=take_first)	
 	return sorted_ledger_list
-
-
--- expense/utils.py
+++ expense/utils.py
@@ -1,6 +1,7 @@
 from .models import Expense
 from django.db.models import Q
 from openpyxl import Workbook
+from django.utils import timezone
 
 def get_expense_from_dates(date1, date2):
     qs = Expense.objects.filter(Q(date_created__gte=date1) & Q(date_created__lte=date2))
@@ -22,3 +23,18 @@ def generate_excel_file_based_on_qs(queryset, response):
     
     wb.save(response)    
     return wb, response
+
+
+def get_today_expense():
+    now = timezone.now()
+    return Expense.objects.filter(date_created=now)
+
+
+def get_this_year_expense():
+    now = timezone.now()
+    return Expense.objects.filter(date_created__year=now.year)
+
+
+def get_this_month_expense():
+    now = timezone.now()
+    return Expense.objects.filter(date_created__year=now.year, date_created__month=now.month)
