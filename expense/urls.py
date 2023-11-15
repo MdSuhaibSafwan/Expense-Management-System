a07ef1bc6a961e1b106bea790c8ba24212aead3e
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-11-16 00:36:41 +0600

    remove previous url for expense report

--- expense/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import create_xl_report_for_expense
-
-urlpatterns = [
-    path("expense-report/", create_xl_report_for_expense, name="expense-report-xl"),
-]
